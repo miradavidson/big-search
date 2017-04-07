360c9a90b2931c8481d68072f9c706f7a52862eb
miradavidson <miraselin@hotmail.com>
2017-04-07 15:13:51 +0200

    Basis of program.

---
+++ main.py
@@ -0,0 +1,21 @@
+# DATABASE INTO STRING
+
+from Bio import SeqIO
+record = SeqIO.read("sampleseq.fasta", "fasta")
+gene = record.format("fasta")
+
+# ALIGNMENT
+# ONLY FINDS THE FIRST MATCH
+# STILL TAKES GENE ID (FIRST LINE) INTO ACCOUNT
+
+def finder(query, data):
+    if query in data:
+        print(data.index(query))
+    else:
+        print("No match found!")
+
+# SEQUENCE INPUT
+
+query_here = input("Enter your query sequence: ")
+
+finder(query_here, gene)
