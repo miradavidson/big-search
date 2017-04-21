9aa3600e4e54a89bb542b918a0de8834133c240a
miradavidson <miraselin@hotmail.com>
2017-04-21 16:23:19 +0100

    Annotations added.

--- main2.py
+++ main2.py
@@ -2,8 +2,8 @@ import pysam
 import timeit
 from multiprocessing import Process, Manager
 
-sequence_filename = "genome_first_split.fasta"
-sequence_name = "1"
+sequence_filename = "sampleseq.fasta"
+sequence_name = "sp|P37840|SYUA_HUMAN"
 
 
 def find_alignment(query, dat, sec_len, proc_offset, rest, all_alignments, k_max):
@@ -16,18 +16,19 @@ def find_alignment(query, dat, sec_len, proc_offset, rest, all_alignments, k_max
     end_point = sec_len * (proc_offset + 1) + rest
     if start_point < 0:
         start_point = 0
-    data = dat[start_point:end_point] # defines the data section per process
+    data = dat[start_point:end_point] # define the data section per process
     m = len(query)
     n = len(data)
 
-    lps = [0] * m
+    lps = [0] * m # create a vector with length of query
     j = 0 # index for query[]
 
-    lps_array(query, m, lps) # preprocesses the query
+    lps_array(query, m, lps) # preprocess the query
 
     i = 0 # index for data[]
     k = 0
     while i < n:
+        # find substring for k = 0
         if query[j] == data[i]:
             i += 1
             j += 1
@@ -35,11 +36,13 @@ def find_alignment(query, dat, sec_len, proc_offset, rest, all_alignments, k_max
             all_alignments.append(start_point + i - j)
             j = lps[j - 1]
             k = 0
+        # find substring with mismatches at end of query
         elif j == m - (k_max - k):
             all_alignments.append(start_point + i - j)
             j = lps[j - 1]
             i += k_max - k
             k = 0
+        # find substring with k mismatches
         elif i < n and query[j] != data[i]:
             if k < k_max:
                 match = False
@@ -62,6 +65,7 @@ def find_alignment(query, dat, sec_len, proc_offset, rest, all_alignments, k_max
                     j = lps[j - 1]
                 else:
                     i += 1
+
 
 def lps_array(query, m, lps):
     """
@@ -90,18 +94,18 @@ if __name__ == '__main__':
     Takes FASTA formatted genome and, given a query, finds substring that match with up to k mismatches.
     """
     samfile = pysam.FastaFile(sequence_filename)
-    genome = samfile.fetch(sequence_name) # fetches each chromosome individually
+    genome = samfile.fetch(sequence_name) # fetch each chromosome individually
 
     num_processes = 4  # define number of processes
-    section_length = int(len(genome) / num_processes) # divides the data for process
+    section_length = int(len(genome) / num_processes) # divide the data into number of processes
     extra = len(genome) % num_processes
 
     manager = Manager()
-    all_alignments = manager.list() # creates a list of indices
+    all_alignments = manager.list() # create a list of indices
 
     query = input("Enter your query sequence: ")
     while not 3 < len(query) < 151:
-        query = input("Please enter a query sequence between 4 and 150 nucleotides: ")
+        query = input("Please enter a query sequence between 4 and 150 bps: ")
     k_max = int(input("Enter the value of k: "))
     procs = []
     start = timeit.default_timer()
@@ -117,5 +121,7 @@ if __name__ == '__main__':
         pass
     stop = timeit.default_timer()
     print("Time taken: {} seconds.".format(stop - start))
-    print(all_alignments)
-    print([genome[i:(i+len(query))] for i in all_alignments])
+    with open('sequence_alignment.txt', 'w') as f:
+        for i in all_alignments:
+            f.write(str(i) + ", " + str(genome[i:(i + len(query))]))
+    f.close()
