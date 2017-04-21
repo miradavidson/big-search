56d2ba02ab88af02a6f0398fbf82b2cc424a7004
miradavidson <miraselin@hotmail.com>
2017-04-21 17:10:01 +0100

    Adapted to use of human genome data.

--- main.py
+++ main.py
@@ -1,24 +1,3 @@
-# DATABASE INTO STRING
-
-from Bio import SeqIO
-record = SeqIO.read("sampleseq.fasta", "fasta")
-gene = record.format("fasta")
-
-# ALIGNMENT
-# ONLY FINDS THE FIRST MATCH
-# STILL TAKES GENE ID (FIRST LINE) INTO ACCOUNT
-
-def findAlignment(query, data):
-    if query in data:
-        print(data.index(query))
-    else:
-        print("No match found!")
-
-# SEQUENCE INPUT
-
-query_here = input("Enter your query sequence: ")
-
-findAlignment(query_here, gene)
-
-with open("sampleseq.fasta", "r+b") as f:
-    mm = mmap.mmap(f.fileno(), 0)
+x = list(range(1,24))
+x.extend(['X', 'Y'])
+print(x)
--- main2.py
+++ main2.py
@@ -1,10 +1,9 @@
 import pysam
 import timeit
 from multiprocessing import Process, Manager
+from time import localtime, strftime
 
-sequence_filename = "sampleseq.fasta"
-sequence_name = "sp|P37840|SYUA_HUMAN"
-
+sequence_filename = "genome_first_split.fasta"
 
 def find_alignment(query, dat, sec_len, proc_offset, rest, all_alignments, k_max):
     """
@@ -94,34 +93,39 @@ if __name__ == '__main__':
     Takes FASTA formatted genome and, given a query, finds substring that match with up to k mismatches.
     """
     samfile = pysam.FastaFile(sequence_filename)
-    genome = samfile.fetch(sequence_name) # fetch each chromosome individually
 
-    num_processes = 4  # define number of processes
-    section_length = int(len(genome) / num_processes) # divide the data into number of processes
-    extra = len(genome) % num_processes
+    chromosome = list(range(1, 2))
+    for c in chromosome: # look at chromosomes individually
+        genome = samfile.fetch(str(c))
+        num_processes = 4  # define number of processes
+        section_length = int(len(genome) / num_processes) # divide the data into number of processes
+        extra = len(genome) % num_processes
 
-    manager = Manager()
-    all_alignments = manager.list() # create a list of indices
+        manager = Manager()
+        all_alignments = manager.list() # create a list of indices
 
-    query = input("Enter your query sequence: ")
-    while not 3 < len(query) < 151:
-        query = input("Please enter a query sequence between 4 and 150 bps: ")
-    k_max = int(input("Enter the value of k: "))
-    procs = []
-    start = timeit.default_timer()
-    leftovers = 0
-    for i in range(num_processes):  # start each of the processes and add to procs list
-        if i == num_processes - 1:
-            leftovers = extra
-        p = Process(target=find_alignment, args=(query, genome, section_length, i, leftovers, all_alignments, k_max))
-        p.start()
-        procs.append(p)
-    print("Started all processes.")
-    while any([p.is_alive() for p in procs]):  # check if any processes are still running
-        pass
-    stop = timeit.default_timer()
-    print("Time taken: {} seconds.".format(stop - start))
-    with open('sequence_alignment.txt', 'w') as f:
-        for i in all_alignments:
-            f.write(str(i) + ", " + str(genome[i:(i + len(query))]))
-    f.close()
+        query = input("Enter your query sequence: ")
+        while not 3 < len(query) < 151:
+            query = input("Please enter a query sequence between 4 and 150 bps: ")
+        k_max = int(input("Enter the value of k: "))
+        procs = []
+        start = timeit.default_timer()
+        leftovers = 0
+        for i in range(num_processes):  # start each of the processes and add to procs list
+            if i == num_processes - 1:
+                leftovers = extra
+            p = Process(target=find_alignment, args=(query, genome, section_length, i, leftovers, all_alignments, k_max))
+            p.start()
+            procs.append(p)
+        print("Started all processes.")
+        while any([p.is_alive() for p in procs]):  # check if any processes are still running
+            pass
+        stop = timeit.default_timer()
+        print("All matches written to file. Time taken: {} seconds.".format(stop - start))
+
+        # write output to file
+        with open('results.txt', 'w') as f:
+            f.write("Chromosome" + str(c) + '\n')
+            for i in sorted(all_alignments, key=int):
+                f.write(str(i) + ", " + str(genome[i:(i + len(query))]) + '\n')
+        f.close()
