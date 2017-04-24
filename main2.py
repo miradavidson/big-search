3a8a07a754d0e8b85937e649a567c484e8542976
miradavidson <miraselin@hotmail.com>
2017-04-25 00:52:35 +0200

    Removed random access.

--- main2.py
+++ main2.py
@@ -33,38 +33,15 @@ def find_alignment(query, dat, sec_len, proc_offset, rest, all_alignments, k_max
             j += 1
         if j == m:
             all_alignments.append(start_point + i - j)
+            print(start_point + i - j)
             j = lps[j - 1]
             k = 0
-        # find substring with mismatches at end of query
-        elif j == m - (k_max - k):
-            all_alignments.append(start_point + i - j)
-            print(start_point+i-j)
-            j = lps[j - 1]
-            i += k_max - k
-            k = 0
-        # find substring with k mismatches
         elif i < n and query[j] != data[i]:
-            if k < k_max:
-                match = False
-                for z in range(1, k_max-k+1):
-                    if j < m-z and i < n-z and query[j+z] == data[i+z]:
-                        i += z
-                        j += z
-                        k += z
-                        match = True
-                        break
-                if not match:
-                    k = 0
-                    if j != 0:
-                        j = lps[j - 1]
-                    else:
-                        i += 1
+            k = 0
+            if j != 0:
+                j = lps[j - 1]
             else:
-                k = 0
-                if j != 0:
-                    j = lps[j - 1]
-                else:
-                    i += 1
+                i += 1
 
 
 def lps_array(query, m, lps):
@@ -88,49 +65,45 @@ def lps_array(query, m, lps):
                 lps[ind] = 0
                 ind += 1
 
-
 if __name__ == '__main__':
     """
     Takes FASTA formatted genome and, given a query, finds substring that match with up to k mismatches.
     """
-    samfile = pysam.FastaFile(sequence_filename)
-
-    chromosome = list(range(1, 23))
-    chromosome.extend(['X', 'Y'])
-
     query = input("Enter your query sequence: ")
     while not 3 < len(query) < 151:
         query = input("Please enter a query sequence between 4 and 150 bps: ")
     k_max = int(input("Enter the value of k: "))
 
-    for c in chromosome: # look at chromosomes individually
-        genome = samfile.fetch(str(c))
-        num_processes = 4  # define number of processes
-        section_length = int(len(genome) / num_processes) # divide the data into number of processes
-        extra = len(genome) % num_processes
+    with pysam.FastxFile(sequence_filename) as fh:
+        for entry in fh:
+            genome = entry.sequence
+            num_processes = 4  # define number of processes
+            section_length = int(len(genome) / num_processes) # divide the data into number of processes
+            extra = len(genome) % num_processes
 
-        manager = Manager()
-        all_alignments = manager.list() # create a list of indices
+            manager = Manager()
+            all_alignments = manager.list() # create a list of indices
 
-        procs = []
-        start = timeit.default_timer()
-        leftovers = 0
-        for i in range(num_processes):  # start each of the processes and add to procs list
-            if i == num_processes - 1:
-                leftovers = extra
-            p = Process(target=find_alignment, args=(query, genome, section_length, i, leftovers, all_alignments, k_max))
-            p.start()
-            procs.append(p)
-        print("Started all processes.")
-        while any([p.is_alive() for p in procs]):  # check if any processes are still running
-            pass
-        stop = timeit.default_timer()
-        print("All matches written to file. Time taken: {} seconds.".format(stop - start))
+            procs = []
+            start = timeit.default_timer()
+            leftovers = 0
+            for i in range(num_processes):  # start each of the processes and add to procs list
+                if i == num_processes - 1:
+                    leftovers = extra
+                p = Process(target=find_alignment, args=(query, genome, section_length, i, leftovers, all_alignments, k_max))
+                p.start()
+                procs.append(p)
+            print("Started all processes.")
+            while any([p.is_alive() for p in procs]):  # check if any processes are still running
+                pass
+            stop = timeit.default_timer()
+            print("All matches written to file. Time taken: {} seconds.".format(stop - start))
 
-        # write output to file
-        with open('results.txt', 'a') as f:
-            if all_alignments != []:
-                f.write("Found on chromosome " + str(c) + '\n')
-                for i in sorted(all_alignments, key=int):
-                    f.write(str(i) + ": " + str(genome[i:(i + len(query))]) + '\n')
-        f.close()
+            # write output to file
+            with open('results.txt', 'a') as f:
+                if all_alignments != []:
+                    f.write("Found on chromosome " + str(entry.name) + '\n')
+                    for i in sorted(all_alignments, key=int):
+                        f.write(str(i) + ": " + str(genome[i:(i + len(query))]) + '\n')
+            f.close()
+            print(len(genome))
